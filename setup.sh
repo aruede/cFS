dc55eac2e9577348bc05ef8d5dae434b28cf3280
aruede <aruede@protonmail.com>
2024-06-02 10:24:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# Get all submodules
 git submodule init
 git submodule update
 
-make SIMULATION=native prep
+# Copy the SBN modules to the apps directory
+./apps/sbn/install .
 
+# Run Cmake to create a new build tree.
+# The native keyword instructs to use the host machine's native compiler.
+make SIMULATION=native prep
